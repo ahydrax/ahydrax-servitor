71c094d4dbc12576e5cc77cc03e45408928c5ae6
Viktor Svyatokha <ahydrax@gmail.com>
2018-09-07 03:29:35 +1000

    Added logs download

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ahydrax.Servitor.Controllers
+{
+    public class LogsController : AuthorizedController
+    {
+        [HttpGet]
+        [Route("/logs")]
+        public async Task<IActionResult> GetLogs()
+        {
+            var logDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+            var directoryInfo = new DirectoryInfo(logDirectoryPath);
+
+            var lastLogfile = directoryInfo.EnumerateFiles("*.txt").OrderBy(x => x.CreationTimeUtc).FirstOrDefault();
+            if (lastLogfile == null) return NotFound();
+
+            var fileBytes = await System.IO.File.ReadAllBytesAsync(lastLogfile.FullName);
+            return File(fileBytes, "application/text", "log.txt");
+        }
+    }
+}
