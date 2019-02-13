4290c3dcec6a01cebff5d55cc4c6e204330a1893
Viktor Svyatokha <ahydrax@gmail.com>
2019-02-14 02:02:46 +1000

    Fixed credentials response sending

--- Actors/TeamspeakCredentialsHolder.cs
+++ Actors/TeamspeakCredentialsHolder.cs
@@ -1,3 +1,4 @@
+using ahydrax.Servitor.Extensions;
 using Akka.Actor;
 
 namespace ahydrax.Servitor.Actors
@@ -15,7 +16,7 @@ namespace ahydrax.Servitor.Actors
         private bool RespondTeamspeakCredentials(MessageArgs arg)
         {
             var message = $"```Host: {_settings.TeamspeakHost}\r\nPassword: {_settings.TeamspeakPublicPassword}`";
-            Context.Sender.Tell(new MessageArgs<string>(arg.ChatId, message));
+            Context.System.SelectActor<TelegramMessageChannel>().Tell(new MessageArgs<string>(arg.ChatId, message));
             return true;
         }
     }
